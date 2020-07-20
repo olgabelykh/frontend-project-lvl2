458ffa55fd9881930973cdca550941216eeab161
olya belykh <belykh.o.v@gmail.com>
2020-07-20 15:38:12 +0300

    stage 2 done

--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,8 @@ const { program } = require('commander');
 const { version, description } = require('./package.json');
 
 const gendiff = () => {
-  program.version(version);
-  program.description(description);
+  program.version(version).description(description).arguments('<filepath1>  <filepath2>');
+  program.option('-f, --format [type]', 'output format');
 
   program.parse(process.argv);
 };
